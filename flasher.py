31c6a2fdff7920815eb8a7dc23cb90c27589d5a8
weining.zhu <zhuwn@juzhongjoy.com>
2018-06-26 16:49:37 +0800

    litter flask program

--- flasher.py
+++ flasher.py
@@ -1,10 +1,15 @@
 #encoding:utf-8
 from flask import Flask
 
+
 app = Flask(__name__)
+#载入配置文件
+app.config.from_object('config')
 
 @app.route('/index')
 def index():
   return "Hello Index"
 
-app.run(host='0.0.0.0', debug=True, port=81)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=app.config['DEBUG'], port=81)
